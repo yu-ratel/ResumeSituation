fc87ee53dfbde925e2b4a47fedbacf2eb9b8b999
yu-ratel <dbdudals100@naver.com>
2024-10-29 16:47:00 +0900

    chore: 기본 폴더 구조 작성

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    '.resume/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    '.resume/components/**/*.{js,ts,jsx,tsx,mdx}',
+    '.resume/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
